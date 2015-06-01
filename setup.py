f24393abff46fa3d331ab255b165550e731e77b0
Matt Brauner <mattbrauner44@gmail.com>
2015-05-31 21:49:51 -0400

    this better work

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     # GETTING-STARTED: set your app name:
     name='marchingtbirds',
     # GETTING-STARTED: set your app version:
-    version='1.0',
+    version='1.1',
     # GETTING-STARTED: set your app description:
     description='App for the Mahwah High School Marching Thunderbirds band',
     # GETTING-STARTED: set author name (your name):
--- wsgi/myproject/marchingtbirds/settings.py
+++ wsgi/myproject/marchingtbirds/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for myproject project.
+Django settings for marchingtbirds project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
